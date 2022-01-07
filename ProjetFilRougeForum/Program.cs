08768e2abeb68002a00eb215b015709be6373b9f
Jerome <chamoussegeroninus@gmail.com>
2022-01-07 13:17:28 +0100

    testage

--- ProjetFilRougeForum/Program.cs
+++ ProjetFilRougeForum/Program.cs
@@ -6,7 +6,7 @@ namespace ProjetFilRougeForum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Wooooorld!");
         }
     }
 }
