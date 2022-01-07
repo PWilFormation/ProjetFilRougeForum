7e8cf3899f6af8a56c65698e949e90e0c721c5c9
PWilFormation <petit.wilfrid.formation@gmail.com>
2022-01-07 11:13:30 +0100

    Ajoutez des fichiers projet.

---
+++ ProjetFilRougeForum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetFilRougeForum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
