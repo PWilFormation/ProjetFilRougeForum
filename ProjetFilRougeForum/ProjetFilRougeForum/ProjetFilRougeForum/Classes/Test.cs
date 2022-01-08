0c324c327b76709144917a944c86c82ce9f6b3e0
PWilFormation <95621033+PWilFormation@users.noreply.github.com>
2022-01-08 20:14:42 +0100

    Add files via upload

---
+++ ProjetFilRougeForum/ProjetFilRougeForum/ProjetFilRougeForum/Classes/Test.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetFilRougeForum.Classes
+{
+    internal class Test
+    {
+        static Dictionary<string, string> Toto = new Dictionary<string, string> { { "Toto", "Titi" }, { "Corentin", "Biroute" }, { "Jerome", "Toto" }, { "Mickael", "Lesboubis" }, { "Wilfrid", "AFK" } };
+        static object[] admin = new object[] { "Admin", "password" };
+
+        public static int TestUser(string name, string password)
+        {
+            if (Convert.ToString(Test.admin[0]) == name)
+            {
+                if (Convert.ToString(Test.admin[1]) == password)
+                {
+                    return 1;
+                }
+            }
+
+            foreach (KeyValuePair<string, string> item in Toto)
+            {
+                if (item.Key == name)
+                {
+                    if (item.Value == password)
+                    {
+                        return 2;
+                    }
+                }
+            }
+            return 0;
+        }
+    }
+}
